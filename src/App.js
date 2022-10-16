0e2c42881d0de090caee3cf2951e20a2ed46ae91
Salim Mvoungui <salim.mvoungui@gmail.com>
2022-10-16 02:06:47 +0200

    menu bar completed full screen

--- src/App.js
+++ src/App.js
@@ -4,12 +4,12 @@ import { Routes, Route } from "react-router-dom";
 import Header from "./components";
 import NotesList from "./containers/notesList";
 import Addnew from "./containers/addList";
+import Nav from "./components/header/header";
 
 function App() {
   return (
     <div className="App">
-      <Header />
-
+      <Nav />
       <Routes>
         <Route path="/" index element={<NotesList />} />
         <Route path="/add-new-note" element={<Addnew />} />
---
+++ src/components/header/header.js
@@ -0,0 +1,61 @@
+import React, { useRef, useState } from "react";
+import "./header.css";
+
+function Nav() {
+  const menuRef = useRef();
+  const [isActive, setActive] = useState("false");
+
+  const handleToggle = () => {
+    setActive((isActive) => !isActive);
+  };
+
+  let toggleClassCheck = isActive ? " is-active" : "";
+  console.log(isActive);
+  return (
+    <>
+      <a className={`btn${toggleClassCheck}`} onClick={handleToggle}>
+        <span></span>
+        <span></span>
+      </a>
+      <div className={`menu${toggleClassCheck}`}>
+        <div className="container bg">
+          <ul className="menu_list">
+            <li className="menu_item">
+              <a href="" className="menu_link">
+                Hem
+              </a>
+            </li>
+            <li className="menu_item">
+              <a href="" className="menu_link">
+                Nyheter
+              </a>
+            </li>
+            <li className="menu_item">
+              <a href="" className="menu_link">
+                verksamheter
+              </a>
+            </li>
+            <li className="menu_item">
+              <a href="" className="menu_link">
+                samarbete
+              </a>
+            </li>
+            <li className="menu_item">
+              <a href="" className="menu_link">
+                kontakt
+              </a>
+            </li>
+          </ul>
+          <div className="right-pic">
+            <img
+              src="https://www.vitarplats.se/img/VI%20TAR%20PLATS-02.png"
+              alt=""
+            />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Nav;
