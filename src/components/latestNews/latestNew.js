ce0476df826b752e3380f42f2931f9f5471de476
Salim Mvoungui <salim.mvoungui@gmail.com>
2022-10-23 01:03:41 +0200

    single page completed

--- src/components/latestNews/latestNew.js
+++ src/components/latestNews/latestNew.js
@@ -3,18 +3,21 @@ import { Link } from "react-router-dom";
 import "./latestNew.css";
 
 function LatestNews(item) {
+  const dateNow = item.post.createdAt;
+  const date = new Date(dateNow);
   return (
     <>
       <div className="leftPicDiv">
         <img src={item.post.pic} alt="" />
       </div>
       <div className="rightContainerDiv">
-        <div className="timestamp">{item.post.createdAt}</div>
-        <h1 className="heading">{item.post.username}</h1>
+        <div className="timestamp">{date.toLocaleDateString()}</div>
+
         <Link to={`/post/${item.post._id}`}>
           {" "}
-          <div className="content">{item.post.desc}</div>
+          <h1 className="heading">{item.post.header}</h1>{" "}
         </Link>
+        <div className="content">{item.post.desc}</div>
       </div>
     </>
   );
--- src/pages/singlePage/singlePage.js
+++ src/pages/singlePage/singlePage.js
@@ -1,23 +1,70 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import axios from "axios";
 import "./singlePage.css";
+import {
+  FacebookIcon,
+  FacebookMessengerIcon,
+  TwitterIcon,
+  LinkedinIcon,
+} from "react-share";
+import {
+  EmailShareButton,
+  FacebookShareButton,
+  InstapaperShareButton,
+  LinkedinShareButton,
+  TwitterShareButton,
+} from "react-share";
 
+import {
+  FacebookShareCount,
+  HatenaShareCount,
+  OKShareCount,
+  PinterestShareCount,
+  RedditShareCount,
+  TumblrShareCount,
+  VKShareCount,
+} from "react-share";
 const SinglePage = () => {
+  const [post, setPost] = useState("");
   const location = useLocation();
-
+  const shareUrl = "https://www.npmjs.com/package/react-share";
   const path = location.pathname.split("/")[2];
 
   useEffect(() => {
     const getPost = async () => {
       const res = await axios.get("http://localhost:5000/api/posts/" + path);
-      console.log(res);
+
+      setPost(res.data);
     };
+
     getPost();
   }, [path]);
+  const dateNow = post.createdAt;
+  const date = new Date(dateNow);
   return (
     <div className="singleContainer">
-      <h1>Salim</h1>
+      {" "}
+      <div className="containerX">
+        <h1>{post.header}</h1>
+        <h3>{post.miniDesc}</h3>
+        <div className="timestamp">{date.toLocaleDateString()}</div>
+        <div className="upperSingleContainer">
+          <img src={post.pic} alt="" className="postPic" />
+          <div className="socialShare">
+            <FacebookShareButton url={shareUrl}>
+              <FacebookIcon size={30} round={true} />
+            </FacebookShareButton>
+            <TwitterShareButton url={shareUrl}>
+              <TwitterIcon size={30} round={true} />
+            </TwitterShareButton>
+            <LinkedinShareButton url={shareUrl}>
+              <LinkedinIcon size={30} round={true} />
+            </LinkedinShareButton>
+          </div>
+        </div>
+        <div className="postText">{post.desc}</div>
+      </div>
     </div>
   );
 };
